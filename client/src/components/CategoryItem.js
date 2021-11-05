81c17dca1948c8c87fe2266c34fa8f72be352831
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-11-05 13:44:33 -0500

    style categories component

--- client/src/components/CategoryItem.js
+++ client/src/components/CategoryItem.js
@@ -3,9 +3,16 @@ import styled from 'styled-components'
 
 
 const Container = styled.div`
+    flex: 1; 
+    margin: 3px; 
+    height: 70vh; 
 `
 
-const Image = styled.img``
+const Image = styled.img`
+    width: 100%; 
+    height: 100%; 
+    object-fit: cover; 
+`
 
 const Info = styled.div``
 
