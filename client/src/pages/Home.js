50860eaa97adc7f325fd5bfe907eac584cfbaa93
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-11-05 11:17:01 -0500

    create navbar componetn

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 function Home() {
     return (
         <div>
-            Homepage
+            <Navbar/>
         </div>
     )
 }
 
 export default Home
+ 
