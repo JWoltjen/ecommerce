dfd46326bedcc8199cb750a92ffe2d6dad64957a
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-11-05 12:54:28 -0500

    style info component

--- client/src/components/Slider.js
+++ client/src/components/Slider.js
@@ -32,12 +32,22 @@ const InfoContainer = styled.div`
 `
 
 const Title = styled.h1`
-
+    font-size: 70px; 
 `
 
-const Description = styled.p``
+const Description = styled.p`
+    margin: 50px 0px; 
+    font-size: 20px; 
+    font-weight: 500; 
+    letter-spacing: 3px; 
+`
 
-const Button = styled.button``
+const Button = styled.button`
+    padding: 10px; 
+    font-size: 20px; 
+    background-color: transparent; 
+    cursor: pointer; 
+`
 
 const Image = styled.img`
     height: 80%; 
@@ -74,9 +84,9 @@ function Slider() {
                         <Image src='https://i.ibb.co/XsdmR2c/1.png'/>
                     </ImageContainer>
                     <InfoContainer>
-                        <Title></Title>
-                        <Description></Description>
-                        <Button></Button>
+                        <Title>Winter Sale</Title>
+                        <Description>When it comes to your style, never compromise</Description>
+                        <Button>Pull the Trigger</Button>
                     </InfoContainer>
                 </Slide>
             </Wrapper>
