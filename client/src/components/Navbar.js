f630dbb65a77c5cbac83c49414748110174b1cf5
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-11-05 11:30:58 -0500

    divide nav components into thirds

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -10,17 +10,25 @@ const Container = styled.div`
 const Wrapper = styled.div`
     padding: 10px 20px; 
     display: flex; 
+    justify-content: space-between; 
 `
 
-const Left = styled.div``
-const Right = styled.div``
-const Center = styled.div``
+const Left = styled.div`
+    width: 33.3%; 
+`
+const Right = styled.div`
+    width: 33.3%; 
+
+`
+const Center = styled.div`
+    width: 33.3%; 
+`
 
 function Navbar() {
     return (
         <Container>
             <Wrapper>
-            <Left>Left</Left>
+            <Left>Tempor proident nulla ea cillum quis pariatur culpa ex do aute voluptate adipisicing. Deserunt culpa eu irure ex ullamco sit eiusmod eiusmod elit cupidatat sint. Cupidatat aute nisi duis sit nostrud cupidatat do ullamco laborum sit adipisicing excepteur labore ut. Commodo eiusmod ut consequat sunt nisi. Ad Lorem consequat adipisicing est irure. Adipisicing mollit reprehenderit consequat laborum consectetur aliqua enim amet consectetur aliquip ipsum proident id.</Left>
             <Center>Center</Center>
             <Right>Right</Right>
             </Wrapper>
