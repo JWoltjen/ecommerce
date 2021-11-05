4f88d12044a64f75bd38a496221b55cdefb2a15c
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-11-05 13:42:54 -0500

    create Categories and Category Item components

---
+++ client/src/components/CategoryItem.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Container = styled.div`
+`
+
+const Image = styled.img``
+
+const Info = styled.div``
+
+const Title = styled.h1``
+
+const Button = styled.button``
+
+
+function CategoryItem({item}) {
+    return (
+        <Container>
+            <Image src={item.img}/>
+            <Info>
+                <Title>
+                    {item.title}
+                </Title>
+                <Button>
+                    SHOP
+                </Button>
+            </Info>
+        </Container>
+    )
+}
+
+export default CategoryItem
