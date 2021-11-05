df1a3443625e23488e2f90b2f08726a08f120483
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-11-05 12:37:35 -0500

    position arrows within slider component

--- client/src/components/Slider.js
+++ client/src/components/Slider.js
@@ -6,17 +6,36 @@ const Container = styled.div`
     width: 100%; 
     height: 100vh; 
     display: flex; 
+    background-color: coral; 
+    position: relative; 
+    overflow: hidden; 
+`
+
+const Arrow = styled.div`
+    width: 50px; 
+    height: 50px; 
+    background-color: white;
+    border-radius: 50%; 
+    display: flex; 
+    align-items: center; 
+    justify-content: center;
+    position: absolute;
+    top: 0; 
+    bottom: 0; 
+    left: ${props => props.direction === 'left' && '10px'}; 
+    right:  ${props => props.direction === 'right' && '10px'}; 
+    margin: auto; 
 `
 
 function Slider() {
     return (
         <Container>
-            <Arrow>
-                <ArrowLeftOutlined/>
+            <Arrow direction="left">
+                <ArrowLeftOutlined/>    
             </Arrow>
-            <Arrow>
-                <ArrowRightOutlined/>
-            </Arrow>
+           <Arrow direction="right">
+               <ArrowRightOutlined/>
+           </Arrow>
 
         </Container>
     )
