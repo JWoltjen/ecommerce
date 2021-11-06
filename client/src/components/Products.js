395ec34b4a43dc6d0ba1982003d179773658dc92
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-11-06 11:09:05 -0500

    add styled components to product component

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -3,15 +3,17 @@ import styled from 'styled-components'
 import { popularProducts } from '../data'
 import Product from './Product'
 
-const Container = styled.div`
 
+const Container = styled.div`
+    padding: 20px; 
+    display: flex; 
 `
 
-const Products = () => {
+const Products = item => {
     return (
         <Container>
             {popularProducts.map(item => (
-                <Product item={item} key={id} />
+                <Product item={item} key={item.id} />
             ))}
         </Container>            
     )
