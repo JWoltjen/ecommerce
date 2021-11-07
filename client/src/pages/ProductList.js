814fa0188a027effbe43be19363ca86504c4d4d6
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-11-07 12:11:02 -0600

    import products newsletter and footer components to shopping page

--- client/src/pages/ProductList.js
+++ client/src/pages/ProductList.js
@@ -1,18 +1,25 @@
 import { Announcement } from '@material-ui/icons'
 import styled from 'styled-components'
+import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
+import Newsletter from '../components/Newsletter'
+import Products from '../components/Products'
 
 const Container = styled.div`
 
 `
 
 const Title = styled.h1`
+    margin: 20px; 
 `
 
-const FilterContainer = styled.div``
+const FilterContainer = styled.div`
+    display: flex; 
+    justify-content: space-between; 
+`
 
 const Filter = styled.div`
-
+    margin: 20px; 
 `
 
 function ProductList() {
@@ -31,6 +38,9 @@ function ProductList() {
                     Filter2
                 </Filter>
             </FilterContainer>
+            <Products/>
+            <Newsletter/>
+            <Footer/>
         </Container>
     )
 }
