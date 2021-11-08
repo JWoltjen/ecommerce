7578d96f774d18d37c71e9cea54593bdf4e7af5c
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-11-08 14:25:05 -0600

    integrate dotenv into index.js

--- api/index.js
+++ api/index.js
@@ -1,9 +1,14 @@
 const express = require('express'); 
 const app = express(); 
 const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 
-mongoose.connect("mongodb+srv://Jeff:Kjw789jj@neumann.reses.mongodb.net/neumann?retryWrites=true&w=majority"
-).then(()=> console.log('db connection successful!')).catch((err)=> console.log(err))
+dotenv.config(); 
+
+mongoose
+.connect(process.env.MONGO_URL)
+.then(()=> console.log('db connection successful!'))
+.catch((err)=> console.log(err))
 
 
 app.listen(5000, () => {
