a416b88d8bef4e0ff3d32672369e72b1efb07a1f
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-11-08 13:10:44 -0600

    connect backend to mongo database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,10 @@
 const express = require('express'); 
 const app = express(); 
+const mongoose = require('mongoose')
+
+mongoose.connect("mongodb+srv://Jeff:Kjw789jj@neumann.reses.mongodb.net/neumann?retryWrites=true&w=majority"
+).then(()=> console.log('db connection successful!')).catch((err)=> console.log(err))
+
 
 app.listen(5000, () => {
     console.log("Backend server is running!")
