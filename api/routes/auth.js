1762419812d421ba67f6ba9f1f7cf7e0654ea6a3
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-11-09 12:27:13 -0600

    setup CryptoJS authentication

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,18 +1,20 @@
 const router = require('express').Router(); 
 const User = require("../models/User")
-//REGISTER
+const CryptoJS = require("crypto-js")
 
+//REGISTER
 router.post("/register", async (req, res)=>{
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password
+        password: CryptoJS.AES.encrypt(req.body.password, "Secret Passphrase")
     });
+
     try{
-   const savedUser = await newUser.save()
-   console.log(savedUser)
-    }catch(err){
-        console.log(err)
+        const savedUser = await newUser.save()
+        res.status(200).json()
+    }  catch(err){
+        res.status(500).json(err); 
     }
 })
 
