4ece795dc355fa5e241009679d4c72c51efc9110
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-11-11 10:34:14 -0600

    create get all products route sortable by new and by category

--- api/routes/product.js
+++ api/routes/product.js
@@ -54,4 +54,28 @@ const router = require('express').Router();
         }
     })
 
+    //GET ALL PRODUCTS 
+    router.get("/",  async (req, res) => {
+        const qNew = req.query.new; 
+        const qCategory = req.query.category; 
+        try{
+            let products; 
+            if(qNew){
+                products = await Product.find().sort({createdAt: -1}.limit(5))
+            } else if(qCategory){
+                products = await Product.find({categories: {
+                    $in:[qCategory], 
+                }})
+            }else{
+                products = await Product.find(); 
+            }
+
+            res.status(200).json(products)
+        } catch (err) {
+            console.log(err); 
+            return
+        }
+    })
+
+
 module.exports = router
