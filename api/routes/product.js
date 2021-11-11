9ff0bdbeaae15c0297906f1a965ab1c37b82cddd
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-11-11 10:18:52 -0600

    build delete product route

--- api/routes/product.js
+++ api/routes/product.js
@@ -11,10 +11,43 @@ const router = require('express').Router();
 
     router.post("/", verifyTokenAndAdmin, async (req, res) => {
         const newProduct = new Product(req.body)
-
         try{
             const savedProduct = await newProduct.save(); 
             res.status(200).json(savedProduct); 
+        }catch(err){
+            console.log(err)
+            return; 
+        }
+    })
+
+    //UPDATE
+    router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+        if(req.body.password){
+                req.body.password = CryptoJS.AES.encrypt(
+                req.body.password, 
+                process.env.PASS_SEC
+            ).toString(); 
+        }
+        try{
+            const updatedProduct = await Product.findByIdAndUpdate(
+            req.params.id, 
+            {
+                $set: req.body
+            }, 
+            { new: true }
+        ); 
+            res.status(200).json(updatedProduct)
+        }catch(err){
+            console.log(err)
+            return 
+        }
+    })
+
+    //DELETE
+    router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+        try{
+            await Product.findByIdAndDelete(req.params.id)
+            res.status(200).json("Product was deleted")
         }catch(err){
             console.log(err)
             return; 
