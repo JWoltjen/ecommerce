97d831a3412826b3491604b5fb68fad23b9e5e47
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-11-11 12:27:37 -0600

    add stripe env key

--- api/index.js
+++ api/index.js
@@ -6,6 +6,8 @@ dotenv.config();
 const userRoute = require("./routes/user")
 const authRoute = require('./routes/auth')
 const productRoute = require('./routes/product')
+const cartRoute = require('./routes/cart')
+const orderRoute = require('./routes/order')
 
 
 
@@ -18,6 +20,8 @@ app.use(express.json())
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
 app.use("/api/products", productRoute)
+app.use('/api/orders', orderRoute)
+app.use('/api/cart/', cartRoute)
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Backend server is running!")
