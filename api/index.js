f2a17d24093e453af7c88a9bb7dedb329e178ad4
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-11-11 12:47:15 -0600

    add cors to index

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config(); 
+const cors = require('cors')
 const userRoute = require("./routes/user")
 const authRoute = require('./routes/auth')
 const productRoute = require('./routes/product')
@@ -16,6 +17,7 @@ mongoose
 .then(()=> console.log('db connection successful!'))
 .catch((err)=> console.log(err)); 
 
+app.use(cors()); 
 app.use(express.json())
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
