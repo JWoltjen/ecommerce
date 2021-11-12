228535b1ce8d34c2dc5905e8de288705bb9cacb4
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-11-12 11:20:32 -0600

    fetch products from products route

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import styled from 'styled-components'
 import { popularProducts } from '../data'
 import Product from './Product'
-
+import axios from 'axios'
 
 const Container = styled.div`
     padding: 20px; 
@@ -12,7 +12,23 @@ const Container = styled.div`
 `
 
 const Products = ({cat, filters, sort }) => {
-    console.log(cat, filters, sort)
+    
+    const [products, setProducts] = useState([]); 
+    const [filteredProducts, setFilteredProducts] = useState([]); 
+
+    useEffect(() => { 
+        const getProducts = async () => {
+            try {
+                const res = await axios.get("http://localhost:5000/api/products")
+                console.log(res)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getProducts(); 
+    }, [cat]); 
+
+
     return (
         <Container>
             {popularProducts.map(item => (
