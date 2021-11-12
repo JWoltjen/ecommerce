cf0fe36311cc659779c7d50bba7383b125dd5e94
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2021-11-12 12:43:10 -0600

    create link to product._id

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,6 +1,7 @@
 import { FavoriteBorderOutlined, SearchOutlined, ShoppingCartOutlined } from '@material-ui/icons'
 import React from 'react'
 import styled from 'styled-components'
+import {Link} from 'react-router-dom'
 
 const Info = styled.div`
     opacity: 0; 
@@ -74,10 +75,15 @@ const Product = ({item}) => {
             <Image src={item.img}/>
             <Info>
                 <Icon>
+                    
                     <ShoppingCartOutlined/>
+                    
                 </Icon>
                 <Icon>
-                    <SearchOutlined/>
+                    <Link to={`/product/${item._id}`}>
+                    
+                        <SearchOutlined/>
+                    </Link>
                 </Icon>
                 <Icon>
                     <FavoriteBorderOutlined/>
